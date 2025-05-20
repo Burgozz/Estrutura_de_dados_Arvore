c9e2c9822da3fb85a6ee460bffffdf05014a3b4c
Computação Unifil <compuni@unifil.br>
2025-05-20 20:17:36 -0300

    contar nós arvore

--- Arvore.java
+++ Arvore.java
@@ -9,6 +9,17 @@ class No {
     }
 }
 
-public class Arvore {
+public class arvore {
     No raiz;
+
+    public int contarNos() {
+        return contarNosRecursivo(raiz);
+    }
+
+    private int contarNosRecursivo(No no) {
+        if (no == null) {
+            return 0;
+        }
+        return 1 + contarNosRecursivo(no.esquerda) + contarNosRecursivo(no.direita);
+    }
 }
