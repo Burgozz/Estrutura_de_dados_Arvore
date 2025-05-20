dfd41639b607dcf365ff0b9c42857e55901c2ed1
Computação Unifil <compuni@unifil.br>
2025-05-20 20:24:48 -0300

    em nivel

--- Arvore.java
+++ Arvore.java
@@ -58,4 +58,26 @@ public class Arvore {
             System.out.print(no.valor + " ");
         }
     }
+    public void emNivel() {
+        if (raiz == null) {
+            return;
+        }
+
+        Queue<No> fila = new LinkedList<>();
+        fila.add(raiz);
+
+        while (!fila.isEmpty()) {
+            No atual = fila.poll();
+            System.out.print(atual.valor + " ");
+
+            if (atual.esquerda != null) {
+                fila.add(atual.esquerda);
+            }
+
+            if (atual.direita != null) {
+                fila.add(atual.direita);
+            }
+        }
+    }
+}
 }
